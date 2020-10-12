ffdedd4bfbe5aa0338bf7391471ccd3fa6bb2cd3
Derek G Chen <derek@derekgc.com>
2020-10-12 08:58:22 -0400

    hw2

--- unit2/hw.py
+++ unit2/hw.py
@@ -8,3 +8,19 @@ print(u, v)
 print(np.transpose(v))
 
 print(np.outer(u, np.transpose(v)))
+
+
+result = [5-24, 7-6, 2-0, 4-12, 3-12, 6-6]
+
+
+def square_error(arr):
+    result = 0
+    for ele in arr:
+        result += ele**2
+    return result/2
+
+
+print(result)
+print(square_error(result))
+
+print(square_error(u) + square_error(v))
