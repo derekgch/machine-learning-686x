44065295db41c588c015fcc031ad793be1df055d
Derek G Chen <derek@derekgc.com>
2020-10-30 20:52:25 -0400

    homework3

---
+++ unit3/HomeWork/cnn.py
@@ -0,0 +1,11 @@
+import numpy as np
+f=[1,3,-1,1,-3]
+g=[1,0,-1]
+
+# print(np.dot([1,3,-1],g))
+# print(np.dot([3,-1,1],g))
+# print(np.dot([-1,1,-3],g))
+
+# print(np.dot([0,1,3],g))
+# print(np.dot([1,-3,0],g))
+
