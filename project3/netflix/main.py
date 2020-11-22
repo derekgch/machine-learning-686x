baf2e74af065ec190230e7fb7e41f73fb2138487
Derek G Chen <derek@derekgc.com>
2020-11-22 16:01:27 -0500

    first kmeans

--- project3/netflix/main.py
+++ project3/netflix/main.py
@@ -5,5 +5,13 @@ import naive_em
 import em
 
 X = np.loadtxt("toy_data.txt")
+K =4
+seeds = [0,1,2,3,4]
+for seed in seeds:
+  mixture, post = common.init(X, K, seed)
+  kmixture, kpost, kcost = kmeans.run(X, mixture, post)
+  title = f"K is {K}, seed is {seed}, cost is {kcost}"
+  print(title)
+  common.plot(X, kmixture, kpost, title)
 
 # TODO: Your code here
